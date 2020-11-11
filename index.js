dfc69cfde99a602bd8e37a7b4cac9aefbcba19f9
Kiri Um <kirirotha@gmail.com>
2020-11-11 21:43:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-let array = [1, -1, 5, 3, -7, 4, 5, 6, -100, 4]
+// let array = [1, -1, 5, 3, -7, 4, 5, 6, -100, 4]
 
 largestSubarraySum = (array) => {
  // code to write here
@@ -7,11 +7,11 @@ largestSubarraySum = (array) => {
  let solutionArray = []
  for(let i=0; i<array.length; i++){
    for(let j=(i+1); j<array.length; j++){
-     let selected = arr.slice(i,(j+1))
+     let selected = array.slice(i,(j+1))
      const reducer = (accumulator, currentValue) => accumulator + currentValue
-     console.log(arr.reduce(reducer))
+     console.log(array.reduce(reducer))
    }
  }
 }
 
-largestSubarraySum(array)
+// largestSubarraySum(array)
